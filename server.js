b903e3ddbe0ce66d335111bc1a4dbd47ef6a1fa3
Daud Semab <daudsemab.dxb@gmail.com>
2020-06-27 22:19:00 +0500

    changed database connection string

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const smartDb = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
